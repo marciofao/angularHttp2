b5f1f11f54f3b5aeea1c267b341a21e19c2c3b5d
Marcio Fao <marcio.lopes.fao@gmail.com>
2026-01-28 20:47:32 -0300

    adding home and housing components

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  template: `
+    <section>
+      <form >
+        <input placeholder="Filter By city">
+        <button class="primary" type="button">
+          Search
+        </button>
+      </form>
+      <section class="results">
+        <app-housing-location></app-housing-location>
+      </section>
+    </section>
+  `,
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}
